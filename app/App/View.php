e2dcb61c8bd24939127131623a3623c9f9ae4f23
rizal <rizal.firdaus117@gmail.com>
2023-02-02 12:18:22 +0700

    Templating View

--- app/App/View.php
+++ app/App/View.php
@@ -7,7 +7,8 @@ class View
 
     public static function render(string $view, $model)
     {
+        require __DIR__ . "/../View/Layouts/header.php";
         require __DIR__ . '/../View/' . $view . '.php';
+        require __DIR__ . "/../View/Layouts/footer.php";
     }
-
 }
