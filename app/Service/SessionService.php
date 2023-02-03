7d1a91b9817388acd60d8da6bd8ffd26d0818c37
rizal <rizal.firdaus117@gmail.com>
2023-02-03 21:54:27 +0700

    Test Session Service Test

--- app/Service/SessionService.php
+++ app/Service/SessionService.php
@@ -5,6 +5,7 @@ namespace ProgrammerZamanNow\Belajar\PHP\MVC\Service;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\Session;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\SessionRepository;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
 
 function setcookie(string $name, string $value)
 {
@@ -43,6 +44,6 @@ class SessionService
         if ($session == null) {
             return null;
         }
-        return $this->userRepository->getById($session->id);
+        return $this->userRepository->getById($session->user_id);
     }
 }
--- test/SessionServiceTest.php
+++ test/SessionServiceTest.php
@@ -4,6 +4,8 @@ namespace ProgrammerZamanNow\Belajar\PHP\MVC;
 
 use PHPUnit\Framework\TestCase;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\Session;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\SessionRepository;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Service\SessionService;
@@ -14,17 +16,58 @@ use ProgrammerZamanNow\Belajar\PHP\MVC\Service\UserService;
 class SessionServiceTest extends TestCase
 {
     private SessionService $sessionService;
-    private UserService $userService;
+    private SessionRepository $sessionRepository;
+    private UserRepository $userRepository;
     public function setUp(): void
     {
-        $session = new SessionRepository(Database::getConnection());
-        $user = new UserRepository(Database::getConnection());
-        $this->sessionService = new SessionService($session, $user);
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+        $this->userRepository = new UserRepository(Database::getConnection());
+        $this->sessionService = new SessionService($this->sessionRepository, $this->userRepository);
+
+        $this->sessionRepository->deleteAll();
+        $this->userRepository->deleteAll();
+
+        $user = new User();
+        $user->setId("rizal");
+        $user->setName("rizal");
+        $user->setPassword(password_hash("rahasia", PASSWORD_BCRYPT));
+        $this->userRepository->save($user);
     }
 
     public function testCreate()
     {
-        $session = $this->sessionService->create("rizal300500");
+        $session = $this->sessionService->create("rizal");
         $this->expectOutputRegex("[X-RIZAL-SESSION : {$session->id}]");
+    }
+    public function testDestroy()
+    {
+        $session = new Session();
+        $session->id = uniqid();
+        $session->user_id = "rizal";
+
+        $result = $this->sessionRepository->save($session);
+        self::assertEquals($session->id, $result->id);
+        $_COOKIE[SessionService::$COOKIE_NAME] = $session->id;
+        $this->expectOutputRegex("[X-RIZAL-SESSION : $session->id]");
+        $this->sessionService->destroy();
+        $this->expectOutputRegex("[X-RIZAL-SESSION : ]");
+    }
+
+    public function testCurrent()
+    {
+        $session = new Session();
+        $session->id = uniqid();
+        $session->user_id = "rizal";
+        $this->sessionRepository->save($session);
+        $_COOKIE[SessionService::$COOKIE_NAME] = $session->id;
+        $user = $this->sessionService->current();
+        self::assertEquals("rizal", $user->getId());
+    }
+
+    public function testCurrentFailed()
+    {
+        $_COOKIE[SessionService::$COOKIE_NAME] = "salah";
+        $user = $this->sessionService->current();
+        self::assertNull($user);
     }
 }
