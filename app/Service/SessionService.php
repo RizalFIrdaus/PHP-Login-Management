1da2252bc4df39f3308d9f28b7ef9a863f1f6e84
rizal <rizal.firdaus117@gmail.com>
2023-02-03 20:25:59 +0700

    Session Service

---
+++ app/Service/SessionService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Service;
+
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\Session;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\SessionRepository;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
+
+class SessionService
+{
+    public static string $COOKIE_NAME = "X-RIZAL-SESSION";
+    public function __construct(
+        private SessionRepository $sessionRepository,
+        private UserRepository $userRepository
+    ) {
+    }
+
+    public function create(string $user_id): Session
+    {
+        $session = new Session();
+        $session->id = uniqid();
+        $session->user_id = $user_id;
+        $session = $this->sessionRepository->save($session);
+        setcookie(self::$COOKIE_NAME, $session->id, time() + (60 * 60 * 24 * 30), "/");
+        return $session;
+    }
+
+    public function destroy(): void
+    {
+        $sessionId = $_COOKIE[self::$COOKIE_NAME] ?? '';
+        $this->sessionRepository->deleteById($sessionId);
+        setcookie(self::$COOKIE_NAME, "", 1, "/");
+    }
+
+    public function current(): ?User
+    {
+        $sessionId = $_COOKIE[self::$COOKIE_NAME] ?? '';
+        $session = $this->sessionRepository->getById($sessionId);
+        if ($session == null) {
+            return null;
+        }
+        return $this->userRepository->getById($session->id);
+    }
+}
